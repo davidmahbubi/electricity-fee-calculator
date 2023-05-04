8a71e75aaa48185b76ee5a7457f98ad254d320be
David Mahbubi <ulrichdavid0370@gmail.com>
2023-05-04 17:00:22 +0700

    Initial Commit

---
+++ Electricity Cost App/Electricity_Cost_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electricity_Cost_AppApp.swift
+//  Electricity Cost App
+//
+//  Created by David Mahbubi on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Electricity_Cost_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
