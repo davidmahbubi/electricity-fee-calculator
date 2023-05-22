3103c17e4cd09c5a49bb1ef6d7dbaf02896b0b30
David Mahbubi <ulrichdavid0370@gmail.com>
2023-05-22 14:58:45 +0700

    Bring back Appliance.swift model

--- Electriwise/Models/Appliance.swift
+++ Electriwise/Models/Appliance.swift
@@ -2,7 +2,17 @@
 //  Appliance.swift
 //  Electriwise
 //
-//  Created by David Mahbubi on 16/05/23.
+//  Created by David Mahbubi on 22/05/23.
 //
 
 import Foundation
+
+struct Appliance: Hashable {
+    var name: String
+    var wattage: UInt16
+    var avgUsage: UInt8
+    var iconName: String
+    var avgUsageUnit: String
+    var avgUsageRepeat: Set<AvgUsageRepeat>
+    var qty: UInt16
+ }
